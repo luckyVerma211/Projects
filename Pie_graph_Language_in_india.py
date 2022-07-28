471688e74e4ec4d6c1f11fd8d59dc7c2138ec9ab
Lucky Verma <109918194+luckyVerma211@users.noreply.github.com>
2022-07-28 23:52:13 +0530

    Add files via upload

---
+++ Pie_graph_Language_in_india.py
@@ -0,0 +1,5 @@
+import matplotlib.pyplot as plt
+a=[33,7,7,14,9,10,20]
+p=["HINDI","URDU","KANNADA","BENGALI","MALAYALAM","TAMIL","OTHERS"]
+plt.pie(a,labels=p,colors=["blue","yellow","red","green","purple","orange","magenta"])
+plt.show()
